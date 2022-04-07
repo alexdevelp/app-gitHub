53178452775d13aa1a4af8e1a7c351b011664cda
Alexandre Pereira <alexandre@placecode.com.br>
2022-04-07 13:23:42 -0300

    Modificando translate da pagina Repositories

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -90,7 +90,8 @@ export const Issues = styled.div`
     transition: transform 0.2s;
 
     &:hover {
-      transform: translateX(6px);
+      transform: translateY(-6px);
+      box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
     }
 
     & + a {
